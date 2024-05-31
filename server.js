adc4a275f80a7bee09cbc4b76dbfeb62f7c6fab3
Ricardo S. Sousa <ricksantos.rs25@gmail.com>
2024-05-30 22:38:25 -0300

    chore: Update server.js to improve code organization and remove unused dependencies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const { Pool } = require("pg");
 const app = express();
 const bcrypt = require("bcrypt");
@@ -21,19 +23,120 @@ const pool = new Pool({
   },
 });
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+const JWT_SECRET =
+  "JwtjPn2oHPe2EYImTkx8BYFAtBmiIdIDaiMEdIuLGSoUpCVLkq+JuMJqOBhqvnElXLHo5MAHlRQVs+V++lw1YTvhBZIHtCleUwWwO3WA+T6nmmhjeZvfNfLWsn1jhvK7eQS57GAG9OkpUdJbCHEqJuhNa5kNOZpjGQbHHdtLN76eVBvriHpk8AL5CT+zrKGI50fe9EwdsqrTiK1/rkJsaKH5WKSS8/05di88wdBGvYGHxivB7Vbp0c0fcsYZ+cfpBOEbC8quw/JPA3YBTJ4WGmNozAetDbQJXyxMYhYeQQIm7vPjgt2jbvZbnOwMCGrVr23lXYZn4ZCjtc7jglvx0A==";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/uploads", express.static(uploadsDir));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Rotas de autenticação e gerenciamento de usuários permanecem inalteradas
+app.post("/api/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to register user" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    const user = result.rows[0];
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { id: user.id, name: user.name, email: user.email },
+        JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to login" });
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
+app.post("/api/users", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+      [name, email]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to add user" });
+  }
+});
+
+app.put("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+      [name, email, id]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to update user" });
+  }
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await pool.query("DELETE FROM users WHERE id = $1", [id]);
+    res.status(204).end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to delete user" });
+  }
+});
 
 app.get("/api/products", async (req, res) => {
   try {
@@ -47,32 +150,13 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-app.get("/api/products/:id/image", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await pool.query(
-      "SELECT image_data FROM products WHERE id = $1",
-      [id]
-    );
-    if (result.rows.length > 0) {
-      res.set("Content-Type", "image/jpeg"); // ou image/png dependendo do tipo da imagem
-      res.send(result.rows[0].image_data);
-    } else {
-      res.status(404).json({ error: "Image not found" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch image" });
-  }
-});
-
 app.post("/api/products", upload.single("image"), async (req, res) => {
   const { name, price } = req.body;
-  const imageData = req.file ? req.file.buffer : null;
+  const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
   try {
     const result = await pool.query(
-      "INSERT INTO products (name, price, image_data) VALUES ($1, $2, $3) RETURNING id, name, price",
-      [name, price, imageData]
+      "INSERT INTO products (name, price, image_url) VALUES ($1, $2, $3) RETURNING *",
+      [name, price, imageUrl]
     );
     res.status(201).json(result.rows[0]);
   } catch (error) {
@@ -84,11 +168,13 @@ app.post("/api/products", upload.single("image"), async (req, res) => {
 app.put("/api/products/:id", upload.single("image"), async (req, res) => {
   const { id } = req.params;
   const { name, price } = req.body;
-  const imageData = req.file ? req.file.buffer : req.body.imageUrl;
+  const imageUrl = req.file
+    ? `/uploads/${req.file.filename}`
+    : req.body.imageUrl;
   try {
     const result = await pool.query(
-      "UPDATE products SET name = $1, price = $2, image_data = $3 WHERE id = $4 RETURNING id, name, price",
-      [name, price, imageData, id]
+      "UPDATE products SET name = $1, price = $2, image_url = $3 WHERE id = $4 RETURNING *",
+      [name, price, imageUrl, id]
     );
     res.json(result.rows[0]);
   } catch (error) {
